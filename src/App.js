40910b23746392b2240a70fe3c664621d04baeb0
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2025-01-19 20:51:03 +0900

    [Add] 메인 컨테이너 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled, {ThemeProvider} from 'styled-components';
+import {theme} from './theme';
 
 export default function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container></Container>
+    </ThemeProvider>
+  );
 }
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: ${({theme}) => theme.background};
+`;
